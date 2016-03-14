736bf8c09569a61faf077e4feb34a97e03f4b54d
Vutisat <vutisat@miamioh.edu>
2016-03-14 14:16:49 -0400

    added resume book

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-header('Location: index.html');
+header('Location: contact.html');
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = "Name: " . $name . "\r\n" . "Reply-To:" . $email . "\r\n" .$_POST['msgtext'];
