6a9f58358e1d19bf45d85dddf8ceee5168ac18d4
Vutisat <vutisat@miamioh.edu>
2016-03-14 13:50:32 -0400

    add contact forms

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php 
+header('Location: index.html');
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = "Name: " . $name . "\r\n" . "Reply-To:" . $email . "\r\n" .$_POST['msgtext'];
+
+$headers = "From: thetataumiami.com" . "\r\n";
+
+$to = "thetatau@miamioh.edu";
+$subject = "New message from the website!";
+
+$a = mail( $to, $subject, $message, $headers );
+if ($a) {
+   echo("Message was sent sucessfully! I look forward to reading it!");
+   exit;
+}else {
+   echo("Message wasn't sent, please check that you have changed emails in the bottom");
+    exit;
+}
